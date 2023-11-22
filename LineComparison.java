8d50b41e861fdcc3db7d5139942d0579b77de563
Exypnos <swaindhruv28@gmail.com>
2023-11-22 13:04:19 +0530

    Refactored: Completed Use case 1

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 
 public class LineComparison {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Line Comparison Computation Program");
+                int x1, y1, x2, y2;
+		Scanner obj = new Scanner(System.in);
+		// User inputs the coordinates
+		System.out.print("Enter x1: ");
+		x1 = obj.nextInt();
+		System.out.print("Enter y1: ");
+		y1 = obj.nextInt();
+		System.out.print("Enter x2: ");
+		x2 = obj.nextInt();
+		System.out.print("Enter y2: ");
+		y2 = obj.nextInt();
+		double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Length of a First Line Segment = " + length1);
 	}
 }
